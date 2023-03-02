609df45ee50f2587fdac465063c38cb2d67bfa20
Shashank Sangale <sangaleshashank2001@gmail.com>
2023-03-02 12:41:25 +0530

    commit after after adding dashboard data from api and sorting done

--- src/component/Dashboard.jsx
+++ src/component/Dashboard.jsx
@@ -4,34 +4,28 @@ import { useNavigate} from 'react-router-dom'
 import Button from '@mui/material/Button';
 import MUIDataTable from "mui-datatables";
 import AuthenticationService from "./AuthenticationService";
+import axios from 'axios'
 
 
 
 const Dashboard=(props)=>{
-    const navigate = useNavigate()
+
+    const[data,setData]  = useState([])
 
     const columns = ["Username","Full Name","Location","Education","Occupation","Hobbies"]
-    const data = [
-        ["jinayshah@gmail.com","Jinay Shah","Mumbai","P.hd","Assistant Professor","Reading, Swimming"],
-        ["shivani@yahoo.com","Shivani Singh","Bangalore","M.com","HR","Dancing"],
-        ["parthpatel@gmail.com","Parth Patel","Delhi","MBA","Finance Professional","Gymming"],
-        ["anandp@rediffmail.com","Anand Pai","Chennai","M.Sc Science","Space Scientist","Sketching"],
-        ["pradnya@gmail.com","Pradnya Velu","Hyderabad","B.Tech","Software Developer","Fitness"]
-    ]
+    
 
+    useEffect(()=>{
+        axios.get("http://localhost:9090/allusers")
+        .then((response)=>{
+            console.log(response.data)
+            setData(response.data)
+        })
+        .catch((error)=>{
+            console.log(error)
+        })
+    })
 
-    // function logout(){
-    //     AuthenticationService.removeUser()
-    //     navigate("/login")
-    // }
-
-    // function profile(){
-    //     navigate("/profile",{state:{data:location.state.data}})
-    // }
-
-    // function aboutus(){
-    //     navigate("/aboutus")
-    // }
 
     return (
         <div>
@@ -43,9 +37,6 @@ const Dashboard=(props)=>{
                 />
             </div>
 
-
-
-            
                
         </div>
         
