baf546faa79e4e2ac0c351163f4a449828a4537a
Shashank Sangale <sangaleshashank2001@gmail.com>
2023-03-06 17:15:57 +0530

    commit after responsiveness is implemented

--- src/components/Aboutus.jsx
+++ src/components/Aboutus.jsx
@@ -3,7 +3,7 @@ import '../assets/styles/AboutusStyle.scss'
 const AboutUs=()=>{
 
     return(
-       <div>
+       <div className="abtus-container">
            <h1 className="header-abtus">About us</h1>
            <big className="big-abtus"> We are Secure employee database management.</big><br/>
            <big className="big-abtus"> We Keep your data safe and serve you whenever you want.   </big>
